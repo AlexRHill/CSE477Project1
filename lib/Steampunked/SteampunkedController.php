e7d846332d017f2d33b39750bea18a61023ad136
nanliu <nan.liu1987@gmail.com>
2016-02-15 13:42:32 -0500

    modify controller

--- lib/Steampunked/SteampunkedController.php
+++ lib/Steampunked/SteampunkedController.php
@@ -12,4 +12,38 @@ namespace Steampunked;
 class SteampunkedController
 {
 
+    /**
+     * Constructor
+     * @param Steampunked $steampunked The Steampunked object
+     * @param $$post The $_POST array
+     */
+    public function __construct(Steampunked $steampunked, $post)
+    {
+        $this->steampunked = $steampunked;
+        if(isset($post['giveup'])) {
+            $this->reset = true;
+            $this->page = "winning.php";
+        } else if(isset($post['rotate'])) {
+            $this->rotate();
+
+        }
+
+    }
+
+    public function getPage() {
+        return $this->page;
+    }
+
+    public function isReset() {
+        return $this->reset;
+    }
+
+    public function rotate() {
+    }
+
+
+    private $steampunked;                // The Steampunked object we are controlling
+    private $page = 'steampuncked.php';     // The next page we will go to
+    private $reset = false;         // True if we need to reset the game
+
 }
