ed1c591ccbb83204f2caaefb66d5e07b687c53a9
helmlin1 <helmlin1@msu.edu>
2016-02-17 00:54:53 -0500

    added model class

--- lib/Steampunked/Steampunked.php
+++ lib/Steampunked/Steampunked.php
@@ -16,7 +16,30 @@ class Steampunked
 
     }
 
+
+
+    //returns the width of the grid
+    public function getWidth(){
+        return $this->width;
+    }
+
+    //returns the height of the grid
+    public function getHeight(){
+        return $this->height;
+    }
+
+    //Returns the turn value
+    public function getTurn(){
+        return $this->turn;
+    }
+
+    //changes the turn from player 1 to 2 or vice versa
+    public function nextTurn(){
+
+    }
+
     private $seed;
     private $width;
     private $height;
+    private $turn;  //whose turn it is
 }
